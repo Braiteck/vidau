ee1f95b222c0421ff13d0af5e0c9d7a8fa473188
Vitaliy Sulimenko <prevetal@gmail.com>
2021-05-18 12:02:39 +0300

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(() => {
+	// Основной слайдер на главной
+	if ($('.reviews .swiper-container').length) {
+		new Swiper('.reviews .swiper-container', {
+			loop: true,
+			speed: 500,
+			watchSlidesVisibility: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			spaceBetween: 30,
+			slidesPerView: 1,
+			pagination: {
+				el: '.swiper-pagination',
+				type: 'bullets',
+				clickable: true,
+				bulletActiveClass: 'active'
+			},
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			}
+		})
+	}
+})
